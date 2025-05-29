634877f27bb4355dbc2e57968fe6d18a750325a8
Luke Meir <27lukemeir@gmail.com>
2025-05-29 16:46:20 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {useEffect} from "react";
+import {useAudioPlayer, useAudioPlayerStatus} from "expo-audio";
+import {Asset} from "expo-asset";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const track = {uri: Asset.fromModule(require('./assets/audio/sound.mp3')).uri};
+
+    const progressUpdateInterval = 500
+
+    const player = useAudioPlayer(track, progressUpdateInterval);
+    const playerStatus = useAudioPlayerStatus(player);
+
+    player.loop = true;
+
+    useEffect(() => {
+        console.log('Current time:', playerStatus.currentTime);
+    }, [playerStatus.currentTime]);
+
+    useEffect(() => {
+        console.log('Player paused:', player.paused);
+        console.log("Player current time is: ", playerStatus.currentTime)
+        console.log("Player playback is: ", playerStatus.reasonForWaitingToPlay)
+    }, [player.paused]);
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity
+                onPress={() => {
+                    if (player.playing) {
+                        player.pause();
+                    } else {
+                        player.play()
+                    }
+                }}
+                style={{padding: 20, backgroundColor: 'blue', borderRadius: 5}}
+            >
+                {player.playing ?
+                    <Text>Pause Audio</Text> :
+                    <Text>Play Audio</Text>}
+            </TouchableOpacity>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
